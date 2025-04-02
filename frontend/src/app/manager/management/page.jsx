2cbafa95f4d436609d5fa69611f77a0bc167c47e
Dean-Ph4m <deanpham2004@tamu.edu>
2025-04-02 14:41:14 -0500

    small fixes

--- frontend/src/app/manager/management/page.jsx
+++ frontend/src/app/manager/management/page.jsx
@@ -1,15 +1,19 @@
 "use client";
-import { useState } from "react";
+import { SERVER } from "@/app/const";
+import { useState, useEffect } from "react";
 import Nav from "@/app/nav";
 
 export default function Management() {
 
-  return (
-    <div className="flex">
-        <Nav userRole="manager" />
-        <div className="bg-white min-h-screen">
-			<h1>Management</h1>
-        </div>
-    </div>
-  );
+  	return (
+    	<div className="flex w-screen">
+        	<Nav userRole="manager"/>
+        	<div className={`flex-1 bg-gradient-to-b from-gray-900 to-gray-700 border-l-6 border-black`}>
+				<h1 className="text-3xl text-left font-bold text-black p-4 text-center bg-neutral-400">
+                	Management Dashboard
+            	</h1>
+
+  			</div>
+    	</div>
+  	);
 }
--- frontend/src/app/manager/reports/page.jsx
+++ frontend/src/app/manager/reports/page.jsx
@@ -1,12 +1,18 @@
 "use client";
+import { SERVER } from "@/app/const";
 import { useState } from "react";
 import Nav from "@/app/nav";
 
 export default function ManagerReport() {
 
-  return (
-    <div>
-        <Nav userRole="manager" />
-    </div>
-  );
+    return (
+    	<div className="flex w-screen">
+        	<Nav userRole="manager"/>
+        	<div className={`flex-1 bg-gradient-to-b from-gray-900 to-gray-700 border-l-6 border-black`}>
+				<h1 className="text-3xl text-left font-bold text-black p-4 text-center bg-neutral-400">
+                	Manage Reports
+            	</h1>
+  			</div>
+    	</div>
+  	);
 }
