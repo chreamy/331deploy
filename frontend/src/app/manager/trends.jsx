65b7a6cd2b18a29fb292ef258c02ccfc56a84079
Dean-Ph4m <deanpham2004@tamu.edu>
2025-04-16 15:33:42 -0500

    manager report generation

--- frontend/src/app/manager/trends.jsx
+++ frontend/src/app/manager/trends.jsx
@@ -6,7 +6,7 @@ import Nav from "@/app/nav";
 export default function Trends() {
     return (
         <div className="overflow-auto h-screen">
-            <div className={`flex-1 bg-gradient-to-b from-gray-900 to-gray-700 border-l-4 border-black`}>
+            <div className={`flex-1 bg-gradient-to-b from-gray-900 to-gray-700 border-black`}>
                 <div className="flex justify-between items-center p-2 bg-white sticky top-0 w-full shadow-md z-50 border-b-black border-b-5">
                     <h1 className="text-3xl text-left font-bold text-black text-center bg-white sticky top-0 w-full">
                         Sales Trends
